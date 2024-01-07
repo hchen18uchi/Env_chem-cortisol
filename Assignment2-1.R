c92a3de0463d7404d27162ab6961cc8f215cb1ff
hchen18uchi <155631555+hchen18uchi@users.noreply.github.com>
2024-01-06 20:50:34 -0600

    assignment2-2

---
+++ Assignment2-1.R
@@ -0,0 +1,9 @@
+library("tidyverse")
+library("papaja")
+## This line is a comment
+## This line is also a comment
+Number_of_cars <- 40
+Number_of_bikes <- 50
+Car_brand <- "BMW"
+Owner <- "car dealer"
+
