842e5a5bc65cff923610bbb93bc7eabd18e16cdf
hchen18uchi <155631555+hchen18uchi@users.noreply.github.com>
2024-01-15 23:36:41 -0600

    helloworld function and YAML update @nrdowling@ysh625

--- Assignment2-1.R
+++ Assignment2-1.R
@@ -7,3 +7,8 @@ Number_of_bikes <- 50
 Car_brand <- "BMW"
 Owner <- "car dealer"
 
+helloworld <- function(firstname,lastname,sex){
+  #Print a greeting line based on name
+  sex <- ifelse( sex == "Female", "Ms.",ifelse( sex == "Male","Mr."))
+  greeting <- paste("Hello", sex,firstname,lastname,"!")
+}
