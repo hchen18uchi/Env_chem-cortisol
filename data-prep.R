53a8aa18509bb909ff71df1076122ce74599d185
hchen18uchi <155631555+hchen18uchi@users.noreply.github.com>
2024-01-23 01:19:57 -0600

    added the data-prep.R file @nrdowling @ysh627

---
+++ data-prep.R
@@ -0,0 +1,4 @@
+#The ideal dataset is pretty close to what I am having right now because my dataset is already a 
+#prepared data. It combined several sepreate dataset from the database including the demographic,
+# the depression measurment and all the urine measurement I might need from the database. The main
+# coding would be select, filter, start with, some calculation and some plot. 
